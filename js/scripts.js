b3efd162c29fb2f3ad261690b658636feae40f6c
Knutsenjamie <Knutsenjamie@yahoo.com>
2021-03-25 16:40:06 -0700

    Add literally everything
    Co-authored-by: Min Chang <minchangmhc@gmail.com>
    Co-authored-by: Michael Burton <micbur1@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,60 @@
+// buisiness logic
+function add(number1, number2) {
+  return number1 + number2;
+};
+
+function subtract(number1, number2) {
+  return number1 - number2;
+};
+
+function multiply(number1, number2) {
+  return number1 * number2;
+};
+
+function divide(number1, number2) {
+  return number1 / number2;
+};
+
+// user logic
+$(document).ready(function() {
+  $("form#calculator").submit(function() {
+    event.preventDefault();
+    const number1 = parseInt($("#input1").val());
+    const number2 = parseInt($("#input2").val());
+    const operator = $("input:radio[name=operator]:checked").val();
+    console.log("1st number: " + number1);  // for debugging
+    console.log("2nd number: " + number2);  // for debugging
+    console.log("operator: " + operator);  // for debugging
+    
+     
+    let result;
+    if (isNaN(number1) || isNaN(number2) ){
+     result ="error";   
+    } else {
+      if (operator === "add") {
+        result = add(number1, number2);                  
+      } else if (operator === "subtract") {
+        result = subtract(number1, number2);
+      } else if (operator === "multiply") {
+        result = multiply(number1, number2);
+      } else if (operator === "divide") {
+        result = divide(number1, number2);
+      }
+    }
+    // if (operator === "add") {
+    //   result = add(number1, number2);
+    // } else if (operator === "subtract") {
+    //   result = subtract(number1, number2);
+    // } else if (operator === "multiply") {
+    //   result = multiply(number1, number2);
+    // } else if (operator === "divide") {
+    //   result = divide(number1, number2);
+    // } else {
+    //   result = "error";
+    // }
+   
+                
+    $("#output").text(result);
+    
+  });
+});  
